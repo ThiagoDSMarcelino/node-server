84cac61dc2f85c9aeac363bdafabfd258cf2a5bf
ThiagoDSMarcelino <thiagodsmarcelino@gmail.com>
2024-02-23 14:19:53 -0300

    Refactor hotel and room models

--- src/routes/AuthRoute.ts
+++ src/routes/AuthRoute.ts
@@ -1,14 +1,11 @@
 import { Router } from 'express';
 
-import container from '../config/container';
-import IAuthService from '../interfaces/IAuthService';
 import authHandler from '../middleware/authHandler';
 
 const AuthRoute = Router();
-const service = container.resolve<IAuthService>('authService');
 
 // Validate
-AuthRoute.post('/', authHandler, async (_, res, __) => {
+AuthRoute.all('/', authHandler, async (_, res, __) => {
 	return res.status(200);
 });
 
