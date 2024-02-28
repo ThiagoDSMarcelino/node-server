a48716a2d3290ea0ff0e43ca597c31c3d144adf8
ThiagoDSMarcelino <thiagodsmarcelino@gmail.com>
2024-02-28 14:33:55 -0300

    Returning user in auth route

--- src/api/routes/AuthRoute.ts
+++ src/api/routes/AuthRoute.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 
+import { user2DTO } from '../../shared/converters';
 import authHandler from '../middleware/authHandler';
 
 const AuthRoute = Router();
 
 // Validate
 AuthRoute.all('/', authHandler, async (_, res, __) => {
-	return res.status(200).json();
+	return res.status(200).json(user2DTO(res.locals.user));
 });
 
 export default AuthRoute;
